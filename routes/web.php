2dee40f5586c0f32c923d290d17521c5087f45e9
m-klis <sasantomuklis@gmail.com>
2023-12-27 16:15:22 +0700

    second commit

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// route resource
+Route::resource('/posts', \App\Http\Controllers\PostController::class);
